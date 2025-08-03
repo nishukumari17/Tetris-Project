406c658bee88acd45bd47bf0621ec9938fa437fc
nishukumari17 <112371617+nishukumari17@users.noreply.github.com>
2025-08-03 13:33:16 +0530

    first commit

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "my-devsecops-project-bucket1"
+    region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+# dynamodb_table = "devsecops-table"
     encrypt        = true
   }
   required_version = ">=0.13.0"
