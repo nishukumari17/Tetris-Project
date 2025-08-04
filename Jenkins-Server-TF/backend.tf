092181877997651078b7228b5c8f2c1de14b4864
nishukumari17 <112371617+nishukumari17@users.noreply.github.com>
2025-08-04 10:34:42 +0530

    update on jenkinsfile

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket = "my-devsecops-project-bucket1"
     region = "ap-south-1"
-    key    = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/.terraform/terraform.tfstate"
-    # dynamodb_table = "devsecops-table"
+    key    = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+     dynamodb_table = "devsecops-table"
     encrypt = true
   }
   required_version = ">=0.13.0"
